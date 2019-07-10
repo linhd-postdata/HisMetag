cf3af9f632f48e86d2b33ee384c81b38dddd151e
M Luisa <ml.diezplatas@linhd.uned.es>
2019-07-10 15:36:47 +0200

    Update GenerateJson.java

--- Hismetag/src/main/java/IOModule/GenerateJson.java
+++ Hismetag/src/main/java/IOModule/GenerateJson.java
@@ -19,10 +19,7 @@ import java.io.FileWriter;
 
 import java.io.IOException;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 
 
 
